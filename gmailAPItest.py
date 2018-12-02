dc8b54c0b14da3f59e87e7b64ba830a0a457cc5e
Akash Anand <36139806+akash9920@users.noreply.github.com>
2018-12-02 17:31:50 -0500

    email with subject

--- gmailAPItest.py
+++ gmailAPItest.py
@@ -1,12 +1,26 @@
 import smtplib
-#from userDefinedVariables import user
 import userDefinedVariables as user
-message = 'This is the sample message to test the api'
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
+subject = 'SafeDoor'
+msg = MIMEMultipart()
+
+msg['From'] = user.user_email
+msg['To'] = user.sender_email
+msg['Subject'] = subject
+
+body = 'This is the sample message to test the api'
+
+msg.attach(MIMEText(body,'plain'))
+
+msgPayload = msg.as_string()
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 server.login(user.user_email, user.user_password)
 
-server.sendmail(user.user_email,user.sender_email,message)
+server.sendmail(user.user_email,user.sender_email,msgPayload)
 
 server.quit()
