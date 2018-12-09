16fd04824270fd87b282f6cd5e2a0eef4449c395
Akash Anand <36139806+akash9920@users.noreply.github.com>
2018-12-08 19:39:26 -0500

    shel

--- InitializeSafeDoor.sh
+++ InitializeSafeDoor.sh
@@ -13,35 +13,5 @@ fi
 
 
 
-python ./SafedoorApp/mqtt.py
-
-if [ $RESULT -eq 0 ]; then
-  echo client is subscribed to the topic SafeDoor
-else
-  echo Failed to intiate the subscriber
-fi
 
 
-
-python SafeDoorCam.py
-
-
-
-
-
-echo "Image capture started"
-
-
-
-echo "Image captured"
-
-if [ $? -eq 0 ]; then
-	echo "calling aws cloud"
-	python 
-	echo "Stack created successfully. Stack Id below: "
-	echo $createOutput
-
-else
-	echo "Error in creation of stack"
-	echo $createOutput
-fi;
