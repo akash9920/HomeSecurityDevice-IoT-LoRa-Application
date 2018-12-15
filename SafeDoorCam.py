088b646ec51355d8e5edf0bfea2ead6fb04933d6
Akash Anand <36139806+akash9920@users.noreply.github.com>
2018-12-15 01:44:10 -0500

    SafeDoorCam lighting added

--- SafeDoorCam.py
+++ SafeDoorCam.py
@@ -1,10 +1,22 @@
 from picamera import PiCamera
 from time import sleep
 
+sense = SenseHat()
 camera = PiCamera()
 
+
+r1 = 255
+g1 = 255
+b1 = 255
+
+r2 = 0
+g2 = 0
+b2 = 0
+
+sense.clear((r1, g1, b1))
 camera.start_preview()
+
 sleep(1)
 camera.capture('image.jpg')
 camera.stop_preview()
-
+sense.clear((r2, g2, b2))
