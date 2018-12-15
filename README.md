ceade1a479f3cfbdc94ea1167494cf383921a6e3
Akash Anand <36139806+akash9920@users.noreply.github.com>
2018-12-15 01:12:25 -0500

    sense hat led

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SafeDoor-IoT-LoRa-Application
+
---
+++ testingSense.py
@@ -0,0 +1,31 @@
+from sense_hat import SenseHat
+
+from sense_hat import SenseHat
+
+# sense = SenseHat()
+
+# O = (255, 255, 255)
+# # X = (0, 0, 0) # Black
+
+# white = [
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+# ]
+
+# sense.set_pixels(white)
+
+# sense.clear()
+
+sense = SenseHat()
+
+r = 255
+g = 255
+b = 255
+
+sense.clear((r, g, b))
